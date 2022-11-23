42564b5aa91de26beed7b8bc3b9749f7524dacd3
yeonjujeong0511 <yeonjujeong0511@gmail.com>
2022-11-23 14:56:35 +0900

    수정사항있었나봄

---
+++ 221101-prac/src/typescript-4.ts
@@ -0,0 +1,19 @@
+// interface 문법은 타입스크립트, 즉 ' 개발'을 위해서만 존재하는 문법으로
+// 아래 실존하는 객체 pokemon과 다르다.
+// interface 방식으로 선언된 PokemonInformation은 '다른곳'에서도 쓰일 수 있기 때문에
+// 따로 선언하는 방식으로 구조가 만들어져있다.
+// 객체를 한번만 쓰는 경우보다 '인스턴스'로 수백수천개를 쓰는 상황이 훨씬 더 많기때문에
+// 인터페이스 작성법은 if문 타입제어를 획기적으로 줄여낼 수 있다.
+// 생성자 함수와 매우 궁합이 좋다.
+
+interface pokemonInformation {
+  id: number;
+  name: string;
+  type: string;
+}
+
+const pokemon: pokemonInformation = {
+  id: 1,
+  name: "이상해씨",
+  type: "풀",
+};
